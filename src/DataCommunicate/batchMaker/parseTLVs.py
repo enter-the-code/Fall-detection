7fcf0a211abd2a0c3bc7139eb916b02f6c0ba59e
F4Corsair <doch2000@naver.com>
2024-11-03 05:43:25 +0900

    new : tlvType - method bind

--- src/DataCommunicate/batchMaker/parseTLVs.py
+++ src/DataCommunicate/batchMaker/parseTLVs.py
@@ -2,14 +2,128 @@ import struct
 import numpy as np
 import math
 
+# Decode 3D People Counting Target List TLV
+# 3D Struct format
+#uint32_t     tid;     /*! @brief   tracking ID */
+#float        posX;    /*! @brief   Detected target X coordinate, in m */
+#float        posY;    /*! @brief   Detected target Y coordinate, in m */
+#float        posZ;    /*! @brief   Detected target Z coordinate, in m */
+#float        velX;    /*! @brief   Detected target X velocity, in m/s */
+#float        velY;    /*! @brief   Detected target Y velocity, in m/s */
+#float        velZ;    /*! @brief   Detected target Z velocity, in m/s */
+#float        accX;    /*! @brief   Detected target X acceleration, in m/s2 */
+#float        accY;    /*! @brief   Detected target Y acceleration, in m/s2 */
+#float        accZ;    /*! @brief   Detected target Z acceleration, in m/s2 */
+#float        ec[16];  /*! @brief   Target Error covariance matrix, [4x4 float], in row major order, range, azimuth, elev, doppler */
+#float        g;
+#float        confidenceLevel;    /*! @brief   Tracker confidence metric*/
+
 def parseTrackTLV(tlvData, tlvLength, outputDict):
-    pass
+    targetStruct = 'I27f'
+    targetSize = struct.calcsize(targetStruct)
+    numDetectedTargets = int(tlvLength/targetSize)
+    targets = np.empty((numDetectedTargets,16))
+    for i in range(numDetectedTargets):
+        try:
+            targetData = struct.unpack(targetStruct,tlvData[:targetSize])
+        except:
+            # parse failed
+            outputDict['numDetectedTracks'], outputDict['trackData'] = 0, targets
+
+        targets[i,0] = targetData[0] # Target ID
+        targets[i,1] = targetData[1] # X Position
+        targets[i,2] = targetData[2] # Y Position
+        targets[i,3] = targetData[3] # Z Position
+        targets[i,4] = targetData[4] # X Velocity
+        targets[i,5] = targetData[5] # Y Velocity
+        targets[i,6] = targetData[6] # Z Velocity
+        targets[i,7] = targetData[7] # X Acceleration
+        targets[i,8] = targetData[8] # Y Acceleration
+        targets[i,9] = targetData[9] # Z Acceleration
+        targets[i,10] = targetData[26] # G
+        targets[i,11] = targetData[27] # Confidence Level
+        
+        # Throw away EC
+        tlvData = tlvData[targetSize:]
+    outputDict['numDetectedTracks'], outputDict['trackData'] = numDetectedTargets, targets
 
 def parseTrackHeightTLV(tlvData, tlvLength, outputDict):
-    pass
+    targetStruct = 'I2f' #incoming data is an unsigned integer for TID, followed by 2 floats
+    targetSize = struct.calcsize(targetStruct)
+    numDetectedHeights = int(tlvLength/targetSize)
+    heights = np.empty((numDetectedHeights,3))
+    for i in range(numDetectedHeights):
+        try:
+            targetData = struct.unpack(targetStruct,tlvData[i * targetSize:(i + 1) * targetSize])
+        except:
+            # tlv parse failed
+            outputDict['numDetectedHeights'], outputDict['heightData'] = 0, heights
+
+        heights[i,0] = targetData[0] # Target ID
+        heights[i,1] = targetData[1] # maxZ
+        heights[i,2] = targetData[2] # minZ
+
+    outputDict['numDetectedHeights'], outputDict['heightData'] = numDetectedHeights, heights
 
 def parseTargetIndexTLV(tlvData, tlvLength, outputDict):
-    pass
+    indexStruct = 'B' # One byte per index
+    indexSize = struct.calcsize(indexStruct)
+    numIndexes = int(tlvLength/indexSize)
+    indexes = np.empty(numIndexes)
+    for i in range(numIndexes):
+        try:
+            index = struct.unpack(indexStruct, tlvData[:indexSize])
+        except:
+            # tlv parse failed
+            outputDict['trackIndexes'] = indexes
+        indexes[i] = int(index[0])
+        tlvData = tlvData[indexSize:]
+    outputDict['trackIndexes'] = indexes
 
 def parseCompressedSphericalPointCloudTLV(tlvData, tlvLength, outputDict):
-    pass
+    pointCloud = outputDict['pointCloud']
+    pUnitStruct = '5f' # Units for the 5 results to decompress them
+    pointStruct = '2bh2H' # Elevation, Azimuth, Doppler, Range, SNR
+    pUnitSize = struct.calcsize(pUnitStruct)
+    pointSize = struct.calcsize(pointStruct)
+
+    # Parse the decompression factors
+    try:
+        pUnit = struct.unpack(pUnitStruct, tlvData[:pUnitSize])
+    except:
+            # parse failed
+            outputDict['numDetectedPoints'], outputDict['pointCloud'] = 0, pointCloud
+    # Update data pointer
+    tlvData = tlvData[pUnitSize:]
+
+    # Parse each point
+    numPoints = int((tlvLength-pUnitSize)/pointSize)
+    for i in range(numPoints):
+        try:
+            elevation, azimuth, doppler, rng, snr = struct.unpack(pointStruct, tlvData[:pointSize])
+        except:
+            numPoints = i
+            # parse failed
+            break
+        
+        tlvData = tlvData[pointSize:]
+        if (azimuth >= 128):
+            # log.error('Az greater than 127')
+            azimuth -= 256
+        if (elevation >= 128):
+            # log.error('Elev greater than 127')
+            elevation -= 256
+        if (doppler >= 32768):
+            # log.error('Doppler greater than 32768')
+            doppler -= 65536
+        # Decompress values
+        pointCloud[i,0] = rng * pUnit[3]          # Range
+        pointCloud[i,1] = azimuth * pUnit[1]      # Azimuth
+        pointCloud[i,2] = elevation * pUnit[0]    # Elevation
+        pointCloud[i,3] = doppler * pUnit[2]      # Doppler
+        pointCloud[i,4] = snr * pUnit[4]          # SNR
+
+    # Convert from spherical to cartesian
+    # pointCloud[:,0:3] = sphericalToCartesianPointCloud(pointCloud[:, 0:3])
+    outputDict['numDetectedPoints'] = numPoints
+    outputDict['pointCloud'] = pointCloud
